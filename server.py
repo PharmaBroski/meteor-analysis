5ed3fdb976d2ba75ec058b41baeae01bb68df6c2
Hashim <hashimahmx@gmail.com>
2018-02-03 16:18:01 -0700

    Added Procedural Inteface

--- server.py
+++ server.py
@@ -6,26 +6,48 @@ host = ''
 port = 5555
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+
 try:
 	socket.bind((host, port))
 except socket.error as e:
-	print(str(e))
+	print(str(e)) #error handling
 
 socket.listen(5)
 print('Waiting for a connection.')
 def threaded_client(conn):
-	conn.send(str.encode('Welcome, to proceed please enter your password\n'))
+
+	ref = 0 #this ref variable keeps track of the user's progress through the terminal interface
+	conn.send(str.encode('Welcome, to proceed please enter your password\nPassword: '))
 
 	while True:
-
-
-		data = conn.recv(2048) #buffer rate
-
-		print(data)
+		data = conn.recv(2048) #2048 is buffer rate
 		reply = data.decode('utf-8')
-		print (reply)
-		if data == b'a\r\n':
-			reply = "YOOOOOO\n"
+
+		#PASSWORD PROTECTION#
+		#	ONCE A USER CONNECTS TO THE SERVER, THEY WILL BE PROMPTED TO ENTER IN A PASSWORD.
+		#	THE USER CAN ONLY ACCESS FEATURES ONCE THE CORRECT PASSWORD IS ENTERED
+		if data == b'research\r\n' and ref == 0: #(utf-8 encoding)
+			reply = "SERVER: You now have access to this server. Type 'help' for assistance or 'exit' to end this session\n"
+			ref = 1
+		if data != b'research\r\n' and ref == 0: #(utf-8 encoding)
+			#if password is incorrect
+			reply = "SERVER: Password incorrect.\n"
+			ref = 0
+
+		#HELP HANDLING
+		#	IF THE USER TYPES IN 'help' INTO THE TERMINAL, A SERIES OF COMMANDS AND THEIR FUNCTIONS WILL BE DISPLAYED
+		#	THE HELP SCREEN CAN ONLY BE ACCESSED IF THE USER DOES NOT
+		if data == b'help\r\n' and ref != 0:
+			reply = "SERVER: HELP STILL BEING BUILT\n"
+
+		#EXIT HANDLING
+		#	IF THE USER TYPES 'exit' THEIR SESSION WILL END.
+		if data == b'exit\r\n': #(utf-8 encoding)
+			reply = "SERVER: Session ending\n"
+			conn.close()
+
+
 		if not data:
 			break
 		conn.sendall(str.encode(reply))
@@ -35,82 +57,7 @@ def threaded_client(conn):
 
 	conn.close()
 	print ('connection lost: ' +addr[0]+':'+str(addr[1]))
-
 while True:
-
 	conn, addr = socket.accept()
 	print('connected to: '+addr[0]+':'+str(addr[1]))
-
 	start_new_thread(threaded_client, (conn,))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import socket
-# import threading
-#
-# sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # tells the socket that it's going to send socket using ipv4 and second param tells to set a tcp connection
-#
-# sock.bind(('0.0.0.0', 10000))
-#
-# sock.listen(1)
-#
-# connections=[]
-#
-# def handler(c, a):
-# 	global connections
-# 	while True:
-# 			data=c.recv(3024)
-# 			for connection in connections
-# 				connection.send(bytes(data))
-# 			if not data:
-# 			connections.remove(c)
-# 			c.close()
-# 			break
-#
-# while True:
-# 	c, a=sock.accept()
-# 	cThread=threading.Thread(target=handler, args=(c,a))
-# 	cThread.daemon=True #program can exit regardless of threads running
-# 	cThread.start()
-# 	connections.append(c)
-# 	print(connections)
