63431d483ab8037980d237d991cbefdc1922f296
Hashim <hashimahmx@gmail.com>
2018-02-03 13:45:01 -0700

    fixed input reading
    Co-Authored-By: Dmitri Vonkeshe <expl0iting@users.noreply.github.com>

--- server.py
+++ server.py
@@ -4,30 +4,41 @@ from _thread import *
 
 host = ''
 port = 5555
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-	s.bind((host, port))
+	socket.bind((host, port))
 except socket.error as e:
 	print(str(e))
 
-s.listen(5)
+socket.listen(5)
 print('Waiting for a connection.')
 def threaded_client(conn):
-	conn.send(str.encode('Welcome, type your info\n'))
+	conn.send(str.encode('Welcome, to proceed please enter your password\n'))
 
 	while True:
+
+
 		data = conn.recv(2048) #buffer rate
-		reply = 'Server output: '+ data.decode('utf-8')
+
+		print(data)
+		reply = data.decode('utf-8')
+		print (reply)
+		if data == b'a\r\n':
+			reply = "YOOOOOO\n"
 		if not data:
 			break
 		conn.sendall(str.encode(reply))
+
+
+
+
 	conn.close()
-	print ('connection lost: ' +addr[0]+':'+str(addr[1]))			
+	print ('connection lost: ' +addr[0]+':'+str(addr[1]))
 
 while True:
 
-	conn, addr = s.accept()
+	conn, addr = socket.accept()
 	print('connected to: '+addr[0]+':'+str(addr[1]))
 
 	start_new_thread(threaded_client, (conn,))
